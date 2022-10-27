76b0224f2df2e3517f0b2426d176bf329ab38a23
yangshengming <smyang92@qq.com>
2022-10-27 13:47:28 +0800

    exit api

--- suanpan/v1/app/app.go
+++ suanpan/v1/app/app.go
@@ -4,16 +4,36 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 
+	"github.com/xuelang-group/suanpan-go-sdk/config"
+	"github.com/xuelang-group/suanpan-go-sdk/suanpan/v1/log"
 	"github.com/xuelang-group/suanpan-go-sdk/suanpan/v1/stream"
 )
 
 func Run(f func(r stream.Request)) {
 	reqs := stream.Subscribe()
 
-	// :6060/debug/pprof
-	go http.ListenAndServe(":6060", nil)
+	go func() {
+		for req := range reqs {
+			go f(req)
+		}
+	}()
 
-	for req := range reqs {
-		go f(req)
-	}
+	done := make(chan struct{}, 1)
+
+	path := "/internal/trap"
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(`{"success": "true"}`))
+			done <- struct{}{}
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(`{"success": "false", "msg": "invalid request"}`))
+		}
+	})
+
+	go http.ListenAndServe(":" + config.GetEnv().SpTermPort, nil)
+
+	<- done
+	log.Info("Exsited")
 }
